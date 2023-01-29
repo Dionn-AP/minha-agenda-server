9b4a268151fd908b5dc35a8ea95e8b2eeece09c3
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-29 01:47:14 -0300

    criação: novas rotas de editar empresa, criar serviços, editar serviços

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import { Router } from "express";
 import Usercontroller from './controllers/user';
 import Passwords from "./controllers/passwords";
 import Login from "./controllers/login";
-import Companiescontroller from './controllers/companies';
+import CompaniesController from './controllers/companies';
+import ServicesController from './controllers/services';
 import authMiddleware from "./middleware/middlewareLogin";
 
 const router = Router();
@@ -14,17 +15,22 @@ router.post('/user', Usercontroller.createuser);
 router.post('/login', Login.authenticate);
 
 //companies
-router.post('/company', Companiescontroller.createcompany);
-router.get('/companies', Companiescontroller.companies);
-router.get('/search-companies', Companiescontroller.searchcompanies);
+router.get('/companies', CompaniesController.companies);
+router.get('/search-companies', CompaniesController.searchcompanies);
+router.post('/company', CompaniesController.createcompany);
+router.patch('/company/:id', CompaniesController.updatecompany);
 
-//authenticated
+//sercives
+router.post('/services', ServicesController.servicescompanies);
+router.patch('/add-services/:id', ServicesController.addservices);
+
+//routes authenticated
 router.get('/user', authMiddleware, Usercontroller.user);
-router.patch('/user', authMiddleware, Usercontroller.update);
-router.delete('/user/:id', authMiddleware, Usercontroller.delete);
 router.post('/user/discover-password', authMiddleware, Passwords.getPassword);
+router.patch('/user', authMiddleware, Usercontroller.update);
 router.patch('/user/change-password', authMiddleware, Passwords.changePassword);
 router.patch('/companies/favorites/:id', authMiddleware, Usercontroller.favoritecompany);
+router.delete('/user/:id', authMiddleware, Usercontroller.delete);
 
 
 export default router;
