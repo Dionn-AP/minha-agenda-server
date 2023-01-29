2531d3c9cd945c4841fd4c6f61d4dff2cdf73bbd
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-29 01:59:09 -0300

    criação: funcoes de criar serviços e adicionar novos serviços

---
+++ src/controllers/services.ts
@@ -0,0 +1,76 @@
+import { Request, Response } from "express";
+import Companies_Service from "../models/Companies_Service";
+import Services from "../models/Services";
+
+class ServicesController {
+    async servicescompanies(req: Request, res: Response) {
+        const { company_id, service_types } = req.body;
+
+        if (!company_id || !service_types.length) {
+            return res.status(400).json({ message: "Você precisa preencher todos os compos obrigatórios" });
+        }
+
+        if (service_types.length < 1) {
+            return res.status(400).json({ message: "Você precisa informar pelo menos um tipo de serviço" });
+        }
+
+        const dataCompany = await Companies_Service.findById({ _id: company_id });
+
+        if (!dataCompany) {
+            return res.status(404).json({ message: "Não foi possível localizar os dados da empresa" });
+        }
+
+        const dataServices = {
+            company_id,
+            name_company: dataCompany?.company!,
+            service_types: service_types
+        }
+
+        try {
+            const servicesCreated = await Services.create(dataServices);
+
+            return res.status(201).json(servicesCreated);
+
+        } catch (error: any) {
+            return res.status(422).json(error);
+        }
+    }
+
+    async addservices(req: Request, res: Response) {
+        const { service_types } = req.body;
+        const { id } = req.params;
+
+        if (service_types.length < 1) {
+            return res.status(400).json({ message: "Você precisa informar pelo menos um tipo de serviço" });
+        }
+
+        const services = await Services.findById({ _id: id });
+
+        if (!services) {
+            return res.status(404).json({ message: "Não foi possível localizar os dados da empresa" });
+        }
+
+        try {
+            if (service_types?.length === 1) {
+                services.service_types?.push(...service_types);
+
+                await Services.updateOne({ _id: id }, services);
+
+                return res.status(201).json({ message: "Serviço adicionado com sucesso" });
+            }
+
+            for (const item of service_types) {
+                services.service_types?.push(item);
+                console.log(services)
+            }
+
+            await Services.updateOne({ _id: id }, services);
+
+            return res.status(201).json({ message: "Serviços adicionados com sucesso" });
+        } catch (error: any) {
+            return res.status(422).json(error);
+        }
+    }
+}
+
+export default new ServicesController();
